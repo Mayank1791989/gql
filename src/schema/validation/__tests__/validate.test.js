881ccc42d417468d05c225a28f613c0e48f43a6f
Mayank Agarwal <mayank1791989@gmail.com>
2018-01-20 00:16:06 +0530

    feat(Schema): add directive validation in schema sdl files.
    closes #93

--- src/schema/validation/__tests__/validate.test.js
+++ src/schema/validation/__tests__/validate.test.js
@@ -65,3 +65,106 @@ it('throw error if rules packages is unknown', () => {
   }).toThrowErrorMatchingSnapshot();
 });
 
+describe('Directiva validation', () => {
+  it('Should report unknown directives', () => {
+    const ast = parse(`
+    type Query {
+      node: String! @some_directive
+    }
+  `);
+
+    const { schema } = buildASTSchema(ast);
+    expect(validate((schema: any), ast)).toMatchSnapshot();
+  });
+
+  it('Should report if directive used in wrong location', () => {
+    const ast = parse(`
+      directive @some_directive on FIELD
+
+      type Query {
+        node: String! @some_directive
+      }
+    `);
+
+    const { schema } = buildASTSchema(ast);
+    expect(validate((schema: any), ast)).toMatchSnapshot();
+  });
+
+  it('Should report if unknown directive arguments used', () => {
+    const ast = parse(`
+      directive @some_directive(
+        boolArg: Boolean
+      ) on FIELD_DEFINITION
+
+      type Query {
+        node: String! @some_directive(test: "5")
+      }
+    `);
+
+    const { schema } = buildASTSchema(ast);
+    expect(validate((schema: any), ast)).toMatchSnapshot();
+  });
+
+  it('Should report if directive argument type is wrong', () => {
+    const ast = parse(`
+      directive @some_directive(
+        boolArg: Boolean
+      ) on FIELD_DEFINITION
+
+      type Query {
+        node: String! @some_directive(boolArg: "5")
+      }
+    `);
+
+    const { schema } = buildASTSchema(ast);
+    expect(validate((schema: any), ast)).toMatchSnapshot();
+  });
+
+  it('Should report if directive required arguments missing', () => {
+    const ast = parse(`
+      directive @some_directive(
+        boolArg: Boolean
+        reqArg: Boolean!
+      ) on FIELD_DEFINITION
+
+      type Query {
+        node: String! @some_directive(boolArg: true)
+      }
+    `);
+
+    const { schema } = buildASTSchema(ast);
+    expect(validate((schema: any), ast)).toMatchSnapshot();
+  });
+
+  it('Should report duplicate arguments', () => {
+    const ast = parse(`
+      directive @some_directive(
+        boolArg: Boolean
+        reqArg: Boolean!
+      ) on FIELD_DEFINITION
+
+      type Query {
+        node: String! @some_directive(boolArg: true, reqArg: true, boolArg: false)
+      }
+    `);
+
+    const { schema } = buildASTSchema(ast);
+    expect(validate((schema: any), ast)).toMatchSnapshot();
+  });
+
+  it('Should report if directive used multiple times in same location', () => {
+    const ast = parse(`
+      directive @some_directive(
+        boolArg: Boolean
+        reqArg: Boolean!
+      ) on FIELD_DEFINITION
+
+      type Query {
+        node: String! @some_directive(reqArg: true) @some_directive(reqArg: false, boolArg: true)
+      }
+    `);
+
+    const { schema } = buildASTSchema(ast);
+    expect(validate((schema: any), ast)).toMatchSnapshot();
+  });
+});
