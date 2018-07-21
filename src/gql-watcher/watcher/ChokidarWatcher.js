4ecadb97c445cef4692cc3b2cd6b3f2a6fd163ae
Mayank Agarwal <mayank1791989@gmail.com>
2018-07-21 16:15:27 +0530

    fix(chokidar): fix chokidar fs watchers not getting closed

--- src/gql-watcher/watcher/ChokidarWatcher.js
+++ src/gql-watcher/watcher/ChokidarWatcher.js
@@ -30,17 +30,17 @@ export default class ChokidarWatcher extends EventsEmitter implements IWatcher {
   };
 
   _setupWatcher = (dir: string, opts: IWatcherOptions) => {
-    return withFixForMissingRemoveEvent(
-      chokidar.watch(opts.glob, {
-        ignored: opts.ignored,
-        ignoreInitial: true,
-        cwd: dir,
-        alwaysStat: true,
-        useFsEvents: process.env.TEST_USE_FS_EVENTS
-          ? parseBool(process.env.TEST_USE_FS_EVENTS)
-          : undefined,
-      }),
-    )
+    const chokidarWatcher = new ChokidarPatched({
+      ignored: opts.ignored,
+      ignoreInitial: true,
+      cwd: dir,
+      alwaysStat: true,
+      useFsEvents: process.env.TEST_USE_FS_EVENTS
+        ? parseBool(process.env.TEST_USE_FS_EVENTS)
+        : undefined,
+    }).add(opts.glob);
+
+    return withFixForMissingRemoveEvent(chokidarWatcher)
       .on('ready', () => {
         this.emit('ready');
       })
@@ -93,4 +93,37 @@ function withFixForMissingRemoveEvent(watcher) {
   });
 
   return watcher;
+}
+
+class ChokidarPatched extends chokidar.FSWatcher {
+  constructor(opts) {
+    super(opts);
+    this._closers = this._fixMultiClosersLost(this._closers);
+  }
+
+  // TEMP_FIX: node process not exiting after watcher.close()
+  // ISSUE: some fs.watch watchers are not closed
+  // Chokidar overriding old closer function with new one
+  // and fs.watch is removed only if all closers are called
+  // causing watcher to stay open when multi closer (listener) added on same path
+  // Below is temp fix to keep all closers and call all closers to successfully
+  // close fs.watch
+  _fixMultiClosersLost = closers => {
+    return new Proxy(closers, {
+      get(target, key) {
+        return target[key];
+      },
+      set(obj, key, closeFn) {
+        if (!obj[key]) {
+          const multiCloseFn = () => {
+            multiCloseFn.fns.forEach(closer => closer());
+          };
+          multiCloseFn.fns = [];
+          obj[key] = multiCloseFn;
+        }
+        obj[key].fns.push(closeFn);
+        return true;
+      },
+    });
+  };
 }
